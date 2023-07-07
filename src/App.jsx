d250b0bdd4203404caf94587c08db0923a035dab
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-07 21:31:39 +0800

    Setup Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route to="/" />
+        </Routes>
+      </Router>
     </>
   )
 }
