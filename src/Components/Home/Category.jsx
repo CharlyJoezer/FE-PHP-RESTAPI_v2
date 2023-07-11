6b95d949d10e4435833dbb60ab11007aedc4edef
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-11 17:03:13 +0800

    Component category halaman home

--- src/Components/Home/Category.jsx
+++ src/Components/Home/Category.jsx
@@ -1,24 +1,37 @@
-import css from "../Home/Category.module.css"
+import css from "../Home/Category.module.css";
+import {Link} from "react-router-dom"
 
 const Category = () => {
   return (
-    <div className={css.categories}>
-      <div className={css.category_item}>
-        <img src="assets/icon/mobile-game.png" alt="mobile-gaming" />
-        <span className={css.category_name}>Gaming</span>
+    <div className={css.categories_wrapper}>
+      <h1 className={css.categories_main_text}>Tentukan Kategori</h1>
+      <div className={css.categories}>
+        <Link className={css.category_item}>
+          <img src="assets/icon/mobile-game.png" alt="mobile-gaming" />
+          <span className={css.category_name}>Mobile Game</span>
+        </Link>
+        <Link className={css.category_item}>
+          <img src="assets/icon/pulsa.png" alt="pulsa" />
+          <span className={css.category_name}>Isi Pulsa</span>
+        </Link>
+        <Link className={css.category_item}>
+          <img src="assets/icon/voucher.png" alt="kode_voucher" />
+          <span className={css.category_name}>Kode Voucher</span>
+        </Link>
+        <Link className={css.category_item}>
+          <img src="assets/icon/game-console.png" alt="game-console" />
+          <span className={css.category_name}>Game Console</span>
+        </Link>
+        <Link className={css.category_item}>
+          <img src="assets/icon/pc-game.png" alt="pc_game" />
+          <span className={css.category_name}>PC Game</span>
+        </Link>
+        <Link className={css.category_item}>
+          <img src="assets/icon/other.png" alt="lainnya" />
+          <span className={css.category_name}>Lainnya</span>
+        </Link>
       </div>
-      <div className={css.category_item}>
-        <img src="assets/icon/pakaian.png" alt="pakaian" />
-        <span className={css.category_name}>Pakaian</span>
-      </div>
-      <div className={css.category_item}>
-        <img src="assets/icon/makanan.png" alt="makanan" />
-        <span className={css.category_name}>Makanan</span>
-      </div>
-      <div className={css.category_item}>
-        <img src="assets/icon/other.png" alt="lainnya" />
-        <span className={css.category_name}>Lainnya</span>
-      </div>
+      <div className={css.hor_line}></div>
     </div>
   );
 };
