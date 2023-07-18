909345d5281ed890cd577fcd4511cd218ec3dedd
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-18 21:54:18 +0800

    Mengubah src image loading

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import css from "../Loading/Loading.module.css"
 const Loading = () => {
     return (
         <div className={css.wrapper_loading}>
-            <img src="assets/gif/loading.gif" alt="" />
+            <img src="/assets/gif/loading.gif" alt="" />
         </div>
     )
 }
