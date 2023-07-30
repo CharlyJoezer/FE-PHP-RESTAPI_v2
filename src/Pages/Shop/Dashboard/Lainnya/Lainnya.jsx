3ea5bf7b2de1fc589fc6ad05659dc20c0d762832
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-31 01:35:22 +0800

    Menambahkan Link Ke halaman utama  website di dashboard shop lainnya

--- src/Pages/Shop/Dashboard/Lainnya/Lainnya.jsx
+++ src/Pages/Shop/Dashboard/Lainnya/Lainnya.jsx
@@ -3,87 +3,46 @@ import css from "./Lainnya.module.css";
 import { Link } from "react-router-dom";
 
 export const Lainnya = () => {
+  const arrowIcon = (
+    <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"></path>
+      </g>
+    </svg>
+  );
   return (
     <>
       <div className={css.container_lainnya}>
         <div className={css.list_link}>
           <Link to="/shop/dashboard/profil-toko" className={css.link_item}>
             <span>Profil Toko</span>
-            <svg
-              fill="#000000"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"></path>
-              </g>
-            </svg>
+            {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/ulasan-pembeli" className={css.link_item}>
             <span>Ulasan Pembeli</span>
-            <svg
-              fill="#000000"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"></path>
-              </g>
-            </svg>
+            {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/pengaturan-toko" className={css.link_item}>
             <span>Pengaturan Toko</span>
-            <svg
-              fill="#000000"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"></path>
-              </g>
-            </svg>
+            {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/produk-toko" className={css.link_item}>
             <span>Produk Toko</span>
-            <svg
-              fill="#000000"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"></path>
-              </g>
-            </svg>
+            {arrowIcon}
           </Link>
-
-
+          <Link to="/" className={css.link_item}>
+            <span>Kembali ke Halaman Utama</span>
+            {arrowIcon}
+          </Link>
         </div>
         <Navbar_Dashboard />
       </div>
