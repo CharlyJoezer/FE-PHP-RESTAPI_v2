4ae02bd5055e9b30def3b71e04d8e11d4f49fb30
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-03 08:11:14 +0800

    Merapikan Code

--- src/Pages/Shop/Dashboard/Beranda/Beranda.jsx
+++ src/Pages/Shop/Dashboard/Beranda/Beranda.jsx
@@ -1,70 +1,72 @@
-import css from "./Beranda.module.css"
-import Navbar_Dashboard from "../../../../Components/Navbar_Dashboard/Navbar_Dashboard"
-import Content from "../../../../Components/Dashboard/Beranda/Content.jsx"
-import Loading from "../../../../Components/Loading/Loading"
-import { useEffect, useState } from "react"
-import BASEURL from "../../../../Utils/baseURL"
-import {Cookie} from "../../../../Auth/Cookies"
-import { useNavigate } from "react-router-dom"
-import ErrorPage from "../../../Errors/ErrorPage.jsx"
+import css from "./Beranda.module.css";
+import Navbar_Dashboard from "../../../../Components/Navbar_Dashboard/Navbar_Dashboard";
+import Content from "../../../../Components/Dashboard/Beranda/Content.jsx";
+import Loading from "../../../../Components/Loading/Loading";
+import { useEffect, useState } from "react";
+import BASEURL from "../../../../Utils/baseURL";
+import { Cookie } from "../../../../Auth/Cookies";
+import { useNavigate } from "react-router-dom";
+import ErrorPage from "../../../Errors/ErrorPage.jsx";
 
 export const Beranda = () => {
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState([]);
-    const [error = {
-        show : false,
-        code : null
-    }, setError] = useState([]);
-    const token = Cookie(' itemku_token')
-    const navigate = useNavigate()
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [
+    error = {
+      show: false,
+      code: null,
+    },
+    setError,
+  ] = useState([]);
+  const token = Cookie(" itemku_token");
+  const navigate = useNavigate();
 
-    useEffect(function(){
-        if(token === undefined){
-            navigate('/login')
-        }else{
-            async function sendRequest(){
-                try{
-                    const url = BASEURL()+"/api/shop/dashboard/home"
-                    const request = await fetch(url,{
-                        method : 'GET',
-                        headers : {
-                            'Authorization' : token
-                        }
-                    })
+  useEffect(function () {
+    if (token === undefined) {
+      navigate("/login");
+    } else {
+      async function sendRequest() {
+        try {
+          const url = BASEURL() + "/api/shop/dashboard/home";
+          const request = await fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: token,
+            },
+          });
 
-                    if(request.status === 200){
-                        const response = await request.json()
-                        setLoading(false)
-                        setData(response)
-                    }else if(request.status === 403){
-                        throw new Error('403')
-                    }else if(request.status === 404){
-                        throw new Error('404')
-                    }
-
-                    }catch(error){
-                        setLoading(false)
-                        setError({show : true, code : isNaN(error.message) ? '500' : error.message})
-                    }
-            }
-            sendRequest()
+          if (request.status === 200) {
+            const response = await request.json();
+            setLoading(false);
+            setData(response);
+          } else if (request.status === 403) {
+            throw new Error("403");
+          } else if (request.status === 404) {
+            throw new Error("404");
+          }
+        } catch (error) {
+          setLoading(false);
+          setError({
+            show: true,
+            code: isNaN(error.message) ? "500" : error.message,
+          });
         }
-    }, [])
-    return (
-        <>
-        {loading
-        ?
-            <Loading />
-        :
-            error.show === true
-            ?
-                <ErrorPage code={error.code} />
-            :
-                <div className={css.container_dashboard_shop}>
-                    <Content data={data}/>
-                    <Navbar_Dashboard />
-                </div>
-        }
-        </> 
-    )
-}
+      }
+      sendRequest();
+    }
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : error.show === true ? (
+        <ErrorPage code={error.code} />
+      ) : (
+        <div className={css.container_dashboard_shop}>
+          <Content data={data} />
+          <Navbar_Dashboard />
+        </div>
+      )}
+    </>
+  );
+};
