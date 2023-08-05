826019ea6b016319a1961cfcf32f4b869dff7b95
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-06 01:40:29 +0800

    menambahkan try catch dan membuat validasi jika terjadi error request

--- src/Pages/Profil/Profil.jsx
+++ src/Pages/Profil/Profil.jsx
@@ -8,6 +8,7 @@ import Footer from "../../Components/Profil/Footer"
 import AuthRequired from "../../Components/AuthRequired/AuthRequired.jsx"
 import Loading from "../../Components/Loading/Loading"
 import BASEURL from "../../Utils/baseURL"
+import ErrorPage from "../../Pages/Errors/ErrorPage"
 
 const Profil = () => {
     document.title = 'Profil'
@@ -15,29 +16,43 @@ const Profil = () => {
     const [user, setDataUser] = useState([])
     const [loading, setLoading] = useState(true)
     const [auth, setAuth] = useState(false)
+    const [errorRequest = {
+      show : false,
+      code : null
+    }, setErrorRequest] = useState([])
 
     useEffect(function(){
       if(token === undefined){
         setLoading(false)
       }else{
         async function getUserData(token){
-          const url = BASEURL()+"/api/auth/user-data"
-          const request = await fetch(url,{
-            method : 'GET',
-            headers : {
-              'Authorization' : token
+          try{
+            const url = BASEURL()+"/api/auth/user-data"
+            const request = await fetch(url,{
+              method : 'GET',
+              headers : {
+                'Authorization' : token
+              }
+            })
+    
+            const response = await request.json()
+    
+            if(request.status === 200){
+              setAuth(true)
+              setDataUser(response.data)
+              setLoading(false)
+            }else if(request.status === 403){
+              setAuth(false)
+              setLoading(false)
+            }else if(request.status === 404){
+              setAuth(false)
+              setLoading(false)
+            }else{
+              throw new Error(500); 
             }
-          })
-  
-          const response = await request.json()
-  
-          if(request.status === 200){
-            setAuth(true)
-            setDataUser(response.data)
-            setLoading(false)
-          }else{
-            setAuth(false)
-            setLoading(false)
+
+          }catch(error){
+            setErrorRequest({show : true, code : isNaN(error.message) ? '500' : error.message})
           }
         }
         getUserData(token)
@@ -45,21 +60,27 @@ const Profil = () => {
     }, [])
     return (
         <>
-            <Navbar users={user}/>
-            <div className={css.container_profil}>
-              {loading ? <Loading /> :
-                auth ?
-                <>
-                  <Header users={user}/>
-                  <Content />
-                  <Footer />
-                </>
+          {errorRequest.show ? 
+            <ErrorPage code={errorRequest.code} />
+            :
+            <>
+              <Navbar users={user}/>
+              <div className={css.container_profil}>
+                {loading ? <Loading /> :
+                  auth ?
+                  <>
+                    <Header users={user}/>
+                    <Content />
+                    <Footer />
+                  </>
 
-                :
+                  :
 
-                <AuthRequired />
-              }
-            </div>
+                  <AuthRequired />
+                }
+              </div>
+            </>
+          }
         </>
     )
 }
