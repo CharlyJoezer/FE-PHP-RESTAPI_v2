660b9d3e5986cc1b46b8ee7e0997b9009389e593
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-06 20:32:23 +0800

    Menmabahkan opsi tampilan jika terjadi error request

--- src/Components/CreateShop/Content.jsx
+++ src/Components/CreateShop/Content.jsx
@@ -4,10 +4,15 @@ import Loading from "../../Components/Loading/Loading"
 import {Cookie} from "../../Auth/Cookies"
 import { useNavigate } from "react-router-dom"
 import BASEURL from "../../Utils/baseURL"
+import ErrorPage from "../../Pages/Errors/ErrorPage"
 
 const Content = () => {
     const [errors, setError] = useState([])
     const [loading, setLoading] = useState(false)
+    const [errorRequest = {
+        show : false,
+        code : null
+    }, setErrorRequest] = useState([])
     const navigate = useNavigate()
 
     function handleFormSubmit(event){
@@ -21,28 +26,35 @@ const Content = () => {
         }
 
         async function sendRequest(){
-            const token = Cookie(' itemku_token')
-            const url = BASEURL()+"/api/shop/create"
-            const request = await fetch(url,{
-                method : 'POST',
-                headers : {
-                    'Authorization' : token,
-                    'Content-Type'  : 'application/json'
-                },
-                body : JSON.stringify({
-                    "name_shop" : inputValue.name_shop
+            try{
+                const token = Cookie(' itemku_token')
+                const url = BASEURL()+"/api/shop/create"
+                const request = await fetch(url,{
+                    method : 'POST',
+                    headers : {
+                        'Authorization' : token,
+                        'Content-Type'  : 'application/json'
+                    },
+                    body : JSON.stringify({
+                        "name_shop" : inputValue.name_shop
+                    })
                 })
-            })
-
-            const response = await request.json()
-            if(request.status === 201){
-                navigate('/shop/dashboard/beranda')
-            }else if(request.status === 422){
-                document.getElementById('name_shop').value = ''
-                setError(response.errors)
+    
+                const response = await request.json()
+                if(request.status === 201){
+                    navigate('/shop/dashboard/beranda')
+                }else if(request.status === 422){
+                    document.getElementById('name_shop').value = ''
+                    setError(response.errors)
+                    setLoading(false)
+                }else if(request.status === 409){
+                    navigate('/shop/dashboard/beranda')
+                }else{
+                    throw new Error("500")
+                }
+            }catch(error){
                 setLoading(false)
-            }else if(request.status === 409){
-                navigate('/shop/dashboard/beranda')
+                setErrorRequest({show : true, code : isNaN(error.message) ? "500" : error.message})
             }
         }
 
@@ -51,21 +63,25 @@ const Content = () => {
     return (
         <>
             {loading && <Loading />}
-            <div className={css.container_content}>
-                <div className={css.content}>
-                    <div className={css.content_image}>
-                        <img src="https://tokoku.itemku.com/static/icon/tokoku-color.png?v=1.26.1" alt="" />
-                    </div>
-                    <form onSubmit={handleFormSubmit} className={css.form_input}>
-                        <div className={css.input}>
-                            <span className={css.input_title}>Nama Toko</span>
-                            <input className={errors.name_shop && css.input_invalid} id="name_shop" name="name_shop" type="text" placeholder="Tokoku" autoComplete="off" required/>
-                            {errors.name_shop && <span style={{color:'red',fontSize: '12px'}}>{errors.name_shop}</span>}
+            {errorRequest.show ? 
+                <ErrorPage code={errorRequest.code} />
+                :
+                <div className={css.container_content}>
+                    <div className={css.content}>
+                        <div className={css.content_image}>
+                            <img src="https://tokoku.itemku.com/static/icon/tokoku-color.png?v=1.26.1" alt="" />
                         </div>
-                        <button className={css.btn_confirm}>Mulai Berjualan</button>
-                    </form>
+                        <form onSubmit={handleFormSubmit} className={css.form_input}>
+                            <div className={css.input}>
+                                <span className={css.input_title}>Nama Toko</span>
+                                <input className={errors.name_shop && css.input_invalid} id="name_shop" name="name_shop" type="text" placeholder="Tokoku" autoComplete="off" required/>
+                                {errors.name_shop && <span style={{color:'red',fontSize: '12px'}}>{errors.name_shop}</span>}
+                            </div>
+                            <button className={css.btn_confirm}>Mulai Berjualan</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            }
         </>
     )
 }
