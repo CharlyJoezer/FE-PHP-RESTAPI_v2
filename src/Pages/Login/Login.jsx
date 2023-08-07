0369f058b48ff0d6997069238cb20db9098e1695
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-07 08:02:59 +0800

    Menambahkan validasi jika user sudah memiliki token akan diredirect di page Login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,15 +2,29 @@ import css from "../Login/Login.module.css";
 import Header from "../../Components/Login/Header.jsx";
 import Content from "../../Components/Login/Content.jsx";
 import Footer from "../../Components/Login/Footer.jsx";
+import { useNavigate } from "react-router-dom";
+import { Cookie } from "../../Auth/Cookies"
+import { useEffect, useState } from "react";
 
 const Login = () => {
+  const getToken = Cookie(' itemku_token')
+  const navigate = useNavigate()
+
+  useEffect(function(){
+    if(getToken !== undefined){
+      navigate('/')
+    }
+  })
+
   return (
     <>
+      {getToken !== undefined &&
       <div className={css.container_login}>
         <Header />
         <Content />
         <Footer />
       </div>
+      }
     </>
   );
 };
