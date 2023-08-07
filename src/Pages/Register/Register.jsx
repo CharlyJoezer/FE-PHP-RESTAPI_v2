3c4b5dc40def031546e0326c60bcd925a55cb770
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-07 08:06:27 +0800

    Menambahkan validasi jika user sudah memiliki token akan diredirect di page Register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,14 +2,30 @@ import css from "../Register/Register.module.css";
 import Header from "../../Components/Register/Header";
 import Content from "../../Components/Register/Content";
 import Footer from "../../Components/Register/Footer";
+import { useNavigate } from "react-router-dom";
+import { Cookie } from "../../Auth/Cookies";
+import { useEffect } from "react";
 
 const Register = () => {
+  const getToken = Cookie(' itemku_token')
+  const navigate = useNavigate()
+
+  useEffect(function(){
+    if(getToken !== undefined){
+      navigate('/')
+    }
+  })
+
   return (
-    <div className={css.container_register}>
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <>
+      {getToken !== undefined &&
+        <div className={css.container_register}>
+          <Header />
+          <Content />
+          <Footer />
+        </div>
+      }
+    </>
   );
 };
 
