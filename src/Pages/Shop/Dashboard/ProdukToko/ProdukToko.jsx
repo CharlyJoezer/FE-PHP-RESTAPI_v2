2faf244df7115add64665f9f87b31b15dd5e93ad
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-13 12:20:31 +0800

    Membuat request untuk mengambil data produk toko

--- src/Pages/Shop/Dashboard/ProdukToko/ProdukToko.jsx
+++ src/Pages/Shop/Dashboard/ProdukToko/ProdukToko.jsx
@@ -2,14 +2,70 @@ import { Header } from "../../../../Components/Dashboard/Components/Header/Heade
 import Navbar_Dashboard from "../../../../Components/Navbar_Dashboard/Navbar_Dashboard"
 import css from "./ProdukToko.module.css"
 import Content from "../../../../Components/Dashboard/ProdukToko/Content"
+import BASEURL from "../../../../Utils/baseURL"
+import {Cookie} from "../../../../Auth/Cookies"
+import { useEffect, useState } from "react"
+import ErrorPage from "../../../Errors/ErrorPage"
+import Loading from "../../../../Components/Loading/Loading"
+import { useNavigate } from "react-router-dom"
 
 export const ProdukToko = () =>{
+    const [product, setProduct] = useState([])
+    const [errorRequest = {
+        show: false,
+        code: null
+    }, setErrorRequest] = useState([])
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate()
+
+
+
+    useEffect(()=>{
+        (async () => {
+            try{
+    
+                const token = Cookie(' itemku_token')
+                const url = BASEURL()+"/api/shop/dashboard/produk-toko"
+                const request = await fetch(url, {
+                    method : "GET",
+                    headers: {
+                        Authorization : token
+                    }
+                })
+        
+                const response = await request.json()
+                if(request.status === 200){
+                    setLoading(false)
+                    setProduct(response.data)
+                }else if(request.status === 400){
+                    setLoading(false)
+                    setErrorRequest({show: true, code: '400' })
+                }else if(request.status === 403){
+                    navigate('/login')
+                }else{
+                    throw new Error("500")
+                }
+            }catch(error){
+                setLoading(false)
+                setErrorRequest({show: true, code: isNaN(error.mesasage) ? "500" : error.message })
+            }
+        })()
+    }, [])
     return (
-        <>
+        <>  
             <div className={css.container_produk_toko}>
-                <Header text="Produk Toko"/>
-                <Content />
-                <Navbar_Dashboard />
+                {loading ? 
+                    <Loading />
+                        :
+                    errorRequest.show ? 
+                    <ErrorPage code={errorRequest.code} />
+                        :
+                    <>
+                    <Header text="Produk Toko"/>
+                    <Content product={product}/>
+                    <Navbar_Dashboard />
+                    </>
+                }
             </div>
         </>
     )
