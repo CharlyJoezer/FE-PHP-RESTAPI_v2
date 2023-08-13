1c237df2a35bc8f87919352e0478ec2d5a9d7070
CharlyJoezer <charlyjoezer30@gmail.com>
2023-08-13 12:21:04 +0800

    Implementasi data request produk toko

--- src/Components/Dashboard/ProdukToko/Content.jsx
+++ src/Components/Dashboard/ProdukToko/Content.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { Modal_Edit } from "./Modal_Edit";
 import { Modal_More } from "./Modal_More";
 import { useState } from "react";
+import { toRupiah } from "../../../Utils/toRupiahFormat";
 
-const Content = () => {
+const Content = (props) => {
+  const product = props.product
   const [modalEdit, setModalEdit] = useState([]);
   const [modalMore, setModalMore] = useState([]);
   return (
@@ -23,61 +25,66 @@ const Content = () => {
         </div>
 
         <div className={css.list_product_shop}>
-          <div className={css.product_shop}>
-            <div className={css.name_product_and_btn}>
-              <span className={css.product_name}>Diamond Lock</span>
-              <svg
-                onClick={() => [
-                  setModalMore({ show: true, data: { id_product: "test" } }),
-                ]}
-                className={css.btn_more}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <circle cx="12" cy="6" r="1.5" fill="#000000"></circle>{" "}
-                  <circle cx="12" cy="12" r="1.5" fill="#000000"></circle>{" "}
-                  <circle cx="12" cy="18" r="1.5" fill="#000000"></circle>{" "}
-                </g>
-              </svg>
-            </div>
-            <div className={css.category_product}>Growtopia</div>
-            <div className={css.price_and_stock_product}>
-              <span className={css.price}>Rp 5.000</span>
-              <span className={css.stock}>0 Lock</span>
-            </div>
-            <div
-              style={{
-                height: "2px",
-                backgroundColor: "#ddd",
-                marginBottom: "12px",
-              }}
-            ></div>
-            <div
-              className={css.btn_edit_product}
-              onClick={() => {
-                setModalEdit({
-                  show: true,
-                  data: {
-                    name: "Diamond Lock",
-                    category: "Growtopia",
-                    price: "5000",
-                    stock: 1,
-                  },
-                });
-              }}
-            >
-              Edit Stok dan Harga
-            </div>
-          </div>
+          {product.map((item) => {
+            return (
+              <div className={css.product_shop} key={item.id_p}>
+                <div className={css.name_product_and_btn}>
+                  <span className={css.product_name}>{item.name}</span>
+                  <svg
+                    onClick={() => [
+                      setModalMore({ show: true, data: { id_product: "test" } }),
+                    ]}
+                    className={css.btn_more}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <circle cx="12" cy="6" r="1.5" fill="#000000"></circle>{" "}
+                      <circle cx="12" cy="12" r="1.5" fill="#000000"></circle>{" "}
+                      <circle cx="12" cy="18" r="1.5" fill="#000000"></circle>{" "}
+                    </g>
+                  </svg>
+                </div>
+                <div className={css.category_product}>{item.sub_categories.name}</div>
+                <div className={css.price_and_stock_product}>
+                  <span className={css.price}>Rp {toRupiah(item.harga.toString())}</span>
+                  <span className={css.stock}>{item.stock} {item.types_sub_categories.name}</span>
+                </div>
+                <div
+                  style={{
+                    height: "2px",
+                    backgroundColor: "#ddd",
+                    marginBottom: "12px",
+                  }}
+                ></div>
+                <div
+                  className={css.btn_edit_product}
+                  onClick={() => {
+                    setModalEdit({
+                      show: true,
+                      data: {
+                        product: item.id_p,
+                        name: item.name,
+                        category: item.sub_categories.name,
+                        price: item.harga.toString(),
+                        stock: item.stock,
+                      },
+                    });
+                  }}
+                >
+                  Edit Stok dan Harga
+                </div>
+              </div>
+            )
+          })}
         </div>
         {modalEdit.show && (
           <div
