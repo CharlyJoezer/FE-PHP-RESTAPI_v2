5908b261e31f327066bd85c34d739df7a03bc8ee
CharlyJoezer <charlyjoezer30@gmail.com>
2023-09-02 17:27:43 +0800

    Edit style dan menambahkan beberapa icon

--- src/Pages/Shop/Dashboard/Lainnya/Lainnya.jsx
+++ src/Pages/Shop/Dashboard/Lainnya/Lainnya.jsx
@@ -21,22 +21,30 @@ export const Lainnya = () => {
       <div className={css.container_lainnya}>
         <div className={css.list_link}>
           <Link to="/shop/dashboard/profil-toko" className={css.link_item}>
-            <span>Profil Toko</span>
+            <span>
+            <img src="/assets/icon/shops2.png" alt="" />
+              Profil Toko</span>
             {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/ulasan-pembeli" className={css.link_item}>
-            <span>Ulasan Pembeli</span>
+            <span>
+            <img src="/assets/icon/star2.png" alt="" />
+              Ulasan Pembeli</span>
             {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/pengaturan-toko" className={css.link_item}>
-            <span>Pengaturan Toko</span>
+            <span>
+              <img src="/assets/icon/gear.png" alt="" />
+              Pengaturan Toko</span>
             {arrowIcon}
           </Link>
 
           <Link to="/shop/dashboard/produk-toko" className={css.link_item}>
-            <span>Produk Toko</span>
+            <span>
+              <img src="/assets/icon/box.png" alt="" />
+              Produk Toko</span>
             {arrowIcon}
           </Link>
           <Link to="/" className={css.link_item}>
--- src/Utils/baseURL.js
+++ src/Utils/baseURL.js
@@ -1,5 +1,6 @@
 const BASEURL = () =>{
-    return "http://127.0.0.1:8000"
+    // return "http://127.0.0.1:8000"
+    return "http://192.168.1.3:8000"
 } 
 
 export default BASEURL;
